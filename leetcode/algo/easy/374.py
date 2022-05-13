50930b54f1d034af0282d8891087f02cc03f8a5d
faisyed <syed.jb.faisal@gmail.com>
2022-05-13 01:41:15 -0400

    work_05_13_22

---
+++ leetcode/algo/easy/374.py
@@ -0,0 +1,12 @@
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l,h = 1, n
+        while l<=h:
+            mid = (l+h)//2
+            if guess(mid)==0:
+                return mid
+            elif guess(mid)==-1:
+                h=mid-1
+            else:
+                l=mid+1
+        
